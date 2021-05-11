5544c08f0bdcadb1ac118b9e94d2e810fbbf193f
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-05-11 19:55:58 -0300

    add delete commands

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,9 @@
+using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        
+    }
+}
