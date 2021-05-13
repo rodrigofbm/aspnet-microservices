d8fd7c3a239b45805b67edb6bee345f5194ce93a
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-05-13 16:30:18 -0300

    sending basket to queue

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,7 +1,11 @@
+using System.Net;
 using System.Threading.Tasks;
+using AutoMapper;
 using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -13,13 +17,17 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly ILogger<BasketController> _logger;
+        private readonly IMapper _mapper;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public BasketController(IBasketRepository repository, ILogger<BasketController> logger, DiscountGrpcService discountGrpcService)
+        public BasketController(IBasketRepository repository, ILogger<BasketController> logger, IMapper mapper, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
             _discountGrpcService = discountGrpcService;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -33,17 +41,15 @@ namespace Basket.API.Controllers
         [HttpPost]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart basket)
         {
-            // TODO: Communicate with Discount.gRPC
-            // TODO: calculate latest price of the product
-            var b = await _repository.UpdateBasket(basket);
-
-            foreach (var item in b.Items)
+            foreach (var item in basket.Items)
             {
                 var discount = await _discountGrpcService.GetDiscount(item.ProductName);
                 item.Price -= discount.Amount;
             }
 
-            return CreatedAtRoute("GetBasket", new {userName = basket.UserName}, b);
+            await _repository.UpdateBasket(basket);
+            
+            return CreatedAtRoute("GetBasket", new {userName = basket.UserName}, basket);
         }
         
         [HttpDelete("{userName}")]
@@ -52,6 +58,28 @@ namespace Basket.API.Controllers
             _repository.DeleteBasket(userName);
 
             return NoContent();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> Checkout(BasketCheckout basketCheckout)
+        {
+            // get existing basket with total price by username
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null) return BadRequest("Basket not found");
+            var totalPrice = basket.TotalPrice;
+            
+            // Create basketCheckoutEvent -- Set total price on basket checkout event
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = totalPrice;
+            
+            // send checkout event to rabbitmq
+            await _publishEndpoint.Publish(eventMessage);
+            
+            // remove the basket
+            _repository.DeleteBasket(basketCheckout.UserName);
+            
+            return Accepted();
         }
     }
 }
